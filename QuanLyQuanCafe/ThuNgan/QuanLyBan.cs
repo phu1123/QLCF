f983ba1a9a202f6f05c0cd557dacd4bb476e6448
nhoxkon095 <nhoxkon095@gmail.com>
2015-12-14 08:23:24 +0700

    UI

--- QuanLyQuanCafe/ThuNgan/QuanLyBan.cs
+++ QuanLyQuanCafe/ThuNgan/QuanLyBan.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Data;
-using System.Data.SqlClient;
 using System.Windows.Forms;
 using BUS;
 
@@ -11,12 +10,30 @@ namespace QuanLyQuanCafe.ThuNgan
         public QuanLyBan()
         {
             InitializeComponent();
+            dataGridView1.DataSource = dtChiTiet;
         }
 
         private void QuanLyBan_Load(object sender, EventArgs e)
         {
             using (HangHoaBUS bus = new HangHoaBUS())
                 dataGridView2.DataSource = bus.ListHangHoa2();
+
+            using (KhuVucBUS bus = new KhuVucBUS())
+            {
+                foreach (DataRow row in bus.ListKhuVuc().Rows)
+                {
+                    var group = new ListViewGroup(row["TenKhuVuc"].ToString());
+                    listView1.Groups.Add(group);
+
+                    foreach (DataRow r in bus.ListBan(row["TenKhuVuc"].ToString()).Rows)
+                        listView1.Items.Add(new ListViewItem(r["TenBan"].ToString(), 0, group));
+                }
+            }
+        }
+
+        private void UpdateTongTien()
+        {
+
         }
 
         private void txtTimKiem_TextChanged(object sender, EventArgs e)
@@ -26,10 +43,31 @@ namespace QuanLyQuanCafe.ThuNgan
 
         private void dataGridView2_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (dataGridView1.CurrentRow == null) return;
+            if (dataGridView2.CurrentRow == null) return;
 
-            string tenhanghoa = dataGridView1.CurrentRow.Cells[0].Value.ToString();
-            //dtChiTiet.Rows.Add(tenhanghoa);
+            string hanghoa = dataGridView2.CurrentRow.Cells[0].Value.ToString();
+            dtChiTiet.Rows.Add(hanghoa);
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            e.Control.KeyPress += dataGridView1_KeyPress;
+        }
+
+        private void dataGridView1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsNumber(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dtChiTiet.Rows.Count == 0) return;
+
+            dtChiTiet.Rows[e.RowIndex].SetField("ThanhTien",
+                Convert.ToInt32(dtChiTiet.Rows[e.RowIndex].Field<string>("DonGia")) *
+                Convert.ToInt32(dtChiTiet.Rows[e.RowIndex].Field<string>("SoLuong")));
+
+            UpdateTongTien();
         }
     }
 }
